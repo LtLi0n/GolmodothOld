d76b11e740d5b0c7283fab844a55e17dd6c317b2
LtLi0n <31516441+LtLi0n@users.noreply.github.com>
2019-08-22 17:24:51 +0300

    Hello World!

---
+++ Golmodoth/Boot/Program.cs
@@ -0,0 +1,13 @@
+﻿using Golmodoth.Engine;
+using System;
+
+namespace Golmodoth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ConsoleWindow().Construct(75, 50, 10, 15, ConsoleGameEngine.FramerateMode.MaxFps);
+        }
+    }
+}
---
+++ Golmodoth/Engine/ConsoleWindow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleGameEngine;
+using static ConsoleGameEngine.ColorPalettes.PaletteColorContainer_DefaultWindowsScheme;
+
+namespace Golmodoth.Engine
+{
+    public class ConsoleWindow : ConsoleGame
+    {
+        public ConsoleWindow()
+        {
+
+        }
+
+        public override void Create()
+        {
+            Engine.SetPalette(PaletteColor.Containers.Default);
+            Engine.Borderless();
+        }
+
+        public override void Render()
+        {
+            Engine.ClearBuffer();
+            Engine.WriteText(new Point(3, 3), "Hello World", FG_GREEN + BG_DARK_BLUE);
+            Engine.DisplayBuffer();
+        }
+
+        public override void Update()
+        {
+
+        }
+    }
+}
